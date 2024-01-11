82c8cbbd3add380f8b88584ee7087ea48a4fc951
IvanK-QX <ivan.k@unitedtech.ai>
2024-01-11 14:29:06 +0200

    change to object model

--- pages/landing_page.js
+++ pages/landing_page.js
@@ -1,36 +1,22 @@
-// class LandingPage {
-//     constructor(driver) {
-//         this.driver = driver;
-//     }
-//     async agreePermissions() {
-//         await $('android.widget.Button').click()
-//         await $('//android.widget.Button[@resource-id="com.android.permissioncontroller:id/permission_allow_button"]').click()
-//         await $('//androidx.compose.ui.platform.l1/android.view.View/android.view.View/android.view.View[3]/android.widget.Button').click()
-//     }
-
-//     get agreeBtn () {
-//         return $('android.widget.Button')
-//     }
-
-//     get allowPermissionBtn() {
-//         return $('//android.widget.Button[@resource-id="com.android.permissioncontroller:id/permission_allow_button"]')
-//     }
-
-//     get phoneIcon() {
-//         return $('//androidx.compose.ui.platform.l1/android.view.View/android.view.View/android.view.View[3]/android.widget.Button')
-//     }
-
-// }
-
-
-// module.exports = new LandingPage()
-
 const LandingPage = {
+    
     async agreePermissions() {
-                await $('android.widget.Button').click()
-                await $('//android.widget.Button[@resource-id="com.android.permissioncontroller:id/permission_allow_button"]').click()
-                await $('//androidx.compose.ui.platform.l1/android.view.View/android.view.View/android.view.View[3]/android.widget.Button').click()
-            }
+        await $('android.widget.Button').click()
+        await $('//android.widget.Button[@resource-id="com.android.permissioncontroller:id/permission_allow_button"]').click()
+        await $('//androidx.compose.ui.platform.l1/android.view.View/android.view.View/android.view.View[3]/android.widget.Button').click()
+    },
+
+    get agreeBtn () {
+        return $('android.widget.Button')
+    },
+
+    get allowPermissionBtn() {
+        return $('//android.widget.Button[@resource-id="com.android.permissioncontroller:id/permission_allow_button"]')
+    },
+
+    get phoneIcon() {
+        return $('//androidx.compose.ui.platform.l1/android.view.View/android.view.View/android.view.View[3]/android.widget.Button')
+    }
     
 }
 
--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -1,26 +1,19 @@
-const landingPage = require("../../pages/landing_page")
-const loginPage = require("../../pages/login_page")
-const mainPage = require("../../pages/main_page")
-const preStreamPage = require("../../pages/prestream_page")
 const app = require("../../pages/app")
+
 describe('MVP', () => {
 
     it('login to the app', async () => {
         await app.landingPage.agreePermissions()
-        // await landingPage.agreeBtn.click()
-        // await landingPage.allowPermissionBtn.click()
-        // await landingPage.phoneIcon.click()
-        await loginPage.phoneNumberField.addValue('3028111111')
-        await loginPage.continieBtn.click()
-        await loginPage.confirmeNumber.click()
-        await loginPage.pressBackbtn()
-        await loginPage.inputeCode()
-        await mainPage.clickStartStream()
-        await preStreamPage.allowPermissions()
-        await preStreamPage.agreeWithTos()
-        await preStreamPage.changeStreamTitle()
-        await preStreamPage.tapStartStreamBtn()
-        await preStreamPage.observeModerationMessage()
+        await app.loginPage.inputPhoneNumber()
+        await app.loginPage.pressBackBtn()
+        await app.loginPage.inputeCode()
+        await app.mainPage.clickStartStream()
+        await app.preStreamPage.allowPermission()
+        await app.preStreamPage.agreeWithTos()
+        await app.preStreamPage.changeStreamTitle()
+        await app.preStreamPage.tapStartStreamBtn()
+        await app.preStreamPage.observeModerationMessage()
+        
         await driver.pause(3000)
     
     })
